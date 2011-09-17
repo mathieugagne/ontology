79ad2df1bc4c3510441471756deca157074a332d
Mathieu Gagné <mathieu@orangebrule.com>
2011-09-17 11:18:20 -0400

    All Tests passed.

--- test/ontology_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OntologyTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Ontology
-  end
-end
--- test/unit/helpers/ontology/types_helper_test.rb
+++
@@ -1,6 +0,0 @@
-require 'test_helper'
-
-module Ontology
-  class TypesHelperTest < ActionView::TestCase
-  end
-end
